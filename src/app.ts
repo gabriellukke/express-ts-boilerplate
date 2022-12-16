45f1daa2da91fb67e4210b4c0697428a86795b79
Gabriel Almeida <gabriel.dev.almeida@outlook.com>
2022-12-16 01:10:00 -0300

    start project

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', async (req: Request, res: Response): Promise<Response> => {
+  return res.status(200).send({
+    message: 'Hello World!',
+  });
+});
+
+export default app;
